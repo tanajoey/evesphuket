e48267866224abf3cc52cb1c481a453d55dc536d
hataipron wannasirot <hataipron.wannasirot@gmail.com>
2023-04-10 19:40:36 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container fluid class="grey lighten-5">
+    <router-view />
+</v-container>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+},
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style>
+@font-face {
+  font-family: "kanitregular";
+  src: url('./assets/fonts/kanit-regular-webfont.woff2') format('woff2'), url('./assets/fonts/kanit-regular-webfont.woff') format('woff');
+}
+body, .v-application{
+  font-family: "kanitregular";
+  font-weight: normal;
+  font-style: normal;
+}
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-container fluid fill-height>
+                <v-layout align-center justify-center>
+                    <v-flex xs12 sm8 md4>
+                        <div class="imgLogo">
+                            <img alt="Vue logo" src="../assets/logo.png">
+                        </div>
+                        <h3>ระบบครุภัณฑ์คอมพิวเตอร์ สำนักงานสาธารณสุขจังหวัดพังงา</h3>
+                        <v-card class="elevation-12">
+                            <v-toolbar dark color="primary">
+                                <v-toolbar-title>เข้าสู่ระบบ</v-toolbar-title>
+                            </v-toolbar>
+                            <v-card-text>
+                                <form ref="form" @submit.prevent="login()">
+                                    <v-text-field v-model="username" name="username" label="ชื่อผู้ใช้" type="text"
+                                        placeholder="ชื่อผู้ใช้" required></v-text-field>
+
+                                    <v-text-field v-model="password" name="password" label="รหัสผ่าน" type="password"
+                                        placeholder="รหัสผ่าน" required></v-text-field>
+                                    <v-btn type="submit" class="mt-4" color="primary" value="log in">เข้าสู่ระบบ</v-btn>
+                                </form>
+                            </v-card-text>
+                        </v-card>
+
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+ 
+<script>
+export default {
+    name: "Log_in",
+    data() {
+        return {
+            username: "",
+            password: "",
+        };
+    },
+    methods: {
+        login() {
+            const { username } = this;
+            this.$router.replace({ name: "dashboard", params: { username: username } });
+        }
+    },
+};
+</script>
+<style scoped>
+.imgLogo {
+    max-width: 100px;
+    margin: 0 auto;
+}
+.imgLogo img {
+    width: 100%;
+}
+</style>
